93a2c06e64a4ce7eaa247f06d546bb26aacc5634
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2020-12-01 09:07:11 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,70 +1,17 @@
-# Getting Started with Create React App
+# Markdown Previewer
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+A React-based app for the [Build a Markdown Previewer](https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer) freeCodeCamp challenge.
 
-## Available Scripts
+## Set up
 
-In the project directory, you can run:
+I wanted to write a React app using Atom with Git version control, but also needed to submit it on freeCodeCamp through CodePen. So, I've set it up with CDNs script tags rather than using the Create-React-App to install a local development server through Webpack. For the same reason, all the React components are kept in a single jsx file.
 
-### `npm start`
+However, I did install Babel locally and configured it to automatically transpile the JSX to JS each time I save the file in Atom.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+## Parsing the Markdown text
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+I'm using the [Marked library](https://github.com/markedjs/marked?utm_source=cdnjs&utm_medium=cdnjs_link&utm_campaign=cdnjs_library) to convert the input from plain text into marked up HTML. Because this outputs a string we need to use the scarey sounding _dangerouslySetInnerHTML_ React attribute to render the HTML elements.
 
-### `npm test`
+From what I've read, using this attribute opens up an XSS attack vunerability and so it is recommended that you sanitize the code first. For this task, I'm using the [DOMPurify library](https://github.com/cure53/DOMPurify).
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+However, I've also been told that the threat of an XSS attack in context of my app is basically non-existent as there is no server or data-sharing involved, so if a user inputs malicious code, it will only affect themselves. Anyway, better to be safe than sorry!
--- src/index.js
+++ src/index.js
@@ -1,17 +1,192 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.scss'
+import marked from 'marked'
+import DOMPurify from 'dompurify'
+
+// default text for the previewer
+const defaultText = `# Welcome to my Markdown Previewer!
+
+## I love h2 headings!
+
+### h3 headings are okay, I guess
+
+This is my first ever **React** app and I built it for a *freeCodeCamp* [Front End Libraries Project](https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer). The app uses ***state*** to automatically update the preview as you type.
+
+Right, that's bold, italic and a link out of the way! Here's a list of random stuff followed by an image of a sweaty Swedish guitar legend:
+* coffee
+* cake
+* types of nut:
+  * brazil
+  * hazelnut
+  * cashew
+    * salted
+    * honey roasted (decadent!)
+    * raw
+* Amiga 1200
+* Yngwie Malmsteen
+
+![Yngwie Malmsteen](https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Yngwie_Malmsteen_1.jpg/220px-Yngwie_Malmsteen_1.jpg)
+
+Here's an inspiring quote from **Avery Brooks**:
+> Knowledge is going to make you stronger. Knowledge is going to let you control your life. Knowledge is going to give you the wisdom to teach their children. Knowledge is the thing that makes you smile in the face of disaster.
+
+Of course, we can also format inline code, such as \`const meaningOfLife = 42;\`, as well as code blocks:
+
+\`\`\`
+// this is some of my React code
+ReactDOM.render(<App />, document.getElementById('root'));
+\`\`\`
+
+Finally, a simple table:
+
+Amiga Game | Developer | No. of floppies
+---------- | --------- | ---------------
+Monkey Island 2: LeChuck's Revenge  | LucasArts | 11
+UFO: Enemy Unknown (AGA)  | Climax | 4
+Cannon Fodder | Sensible Software | 3
+Syndicate | Bullfrog | 4
+
+Oh, I almost forgot that there's an optional user story for getting the previewer to render line
+breaks like this one with a \`<br>\`!
+
+This last part is to test the behaviour of the preview box with very long words:
+
+blahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblah...
+`
+
+// set options for Marked
+marked.setOptions({
+  breaks: true, // adds <br> on single line breaks
+  headerIds: false // disables auto header ids
+})
+
+function Header() {
+  return (
+    <header>
+      <h1 className="text-center">Markup my Markdown!</h1>
+    </header>
+  )
+}
+
+class Input extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      rawText: defaultText
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.getMarkdown = this.getMarkdown.bind(this)
+  }
+
+  getMarkdown() {
+    // parse markdown text using Marked library
+    const htmlMarkup = marked(this.state.rawText)
+
+    // clean up HTML to mitigate XSS risk
+    const cleanMarkup = DOMPurify.sanitize(htmlMarkup)
+
+    // prepare html for use with dangerouslySetInnerHTML attribute
+    return { __html: cleanMarkup }
+  }
+
+  handleChange(event) {
+    this.setState({
+      rawText: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Editor handleChange={this.handleChange} rawText={this.state.rawText} />
+        <Preview getMarkdown={this.getMarkdown()} />
+      </div>
+    )
+  }
+}
+
+function Editor(props) {
+  return (
+    <div className="flexitem">
+      <div className="bar">
+        <p>Input window</p>
+      </div>
+      <textarea
+        id="editor"
+        value={props.rawText}
+        spellCheck="false"
+        onChange={props.handleChange}
+      />
+    </div>
+  )
+}
+
+function Preview(props) {
+  return (
+    <div className="flexitem">
+      <div className="bar">
+        <p>Preview window</p>
+      </div>
+      <div id="preview" dangerouslySetInnerHTML={props.getMarkdown}></div>
+    </div>
+  )
+}
+
+function Footer() {
+  return (
+    <footer className="text-center">
+      <p>
+        Written, coded and abused by&nbsp;
+        <a
+          href="https://steviegill-webportfolio.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Stevie Gill
+        </a>
+      </p>
+      <p>
+        Made with{' '}
+        <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
+          React
+        </a>
+        ,{' '}
+        <a
+          href="https://github.com/markedjs/marked"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Marked
+        </a>{' '}
+        and{' '}
+        <a
+          href="https://github.com/cure53/DOMPurify"
+          target="_blank"
+          rel="noreferrer"
+        >
+          DOMPurify
+        </a>
+      </p>
+    </footer>
+  )
+}
+
+function App() {
+  return (
+    <div className="outer">
+      <Header />
+      <Input />
+      <Footer />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)

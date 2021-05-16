d0b304186865ef0df53c1f310b02ff6d3ee8ad07
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-05-16 10:09:10 -0400

    Updated footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,35 +4,13 @@ function Footer() {
   return (
     <footer className="text-center">
       <p>
-        Written, coded and abused by&nbsp;
+        Coded by&nbsp;
         <a
           href="https://steviegill-webportfolio.netlify.app/"
           target="_blank"
           rel="noreferrer"
         >
           Stevie Gill
-        </a>
-      </p>
-      <p>
-        Made with{' '}
-        <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
-          React
-        </a>
-        ,{' '}
-        <a
-          href="https://github.com/markedjs/marked"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Marked
-        </a>{' '}
-        and{' '}
-        <a
-          href="https://github.com/cure53/DOMPurify"
-          target="_blank"
-          rel="noreferrer"
-        >
-          DOMPurify
         </a>
       </p>
     </footer>

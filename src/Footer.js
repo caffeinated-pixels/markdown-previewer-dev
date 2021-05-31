087247ae513cdfaa9fcf05c1130e0267c9f79f59
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-05-31 10:08:06 -0400

    add font awesome and github repo link to footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons'
 
 function Footer() {
   return (
@@ -7,10 +9,20 @@ function Footer() {
         Coded by&nbsp;
         <a
           href="https://steviegill-webportfolio.netlify.app/"
+          title="Stevie Gill's portfolio page"
           target="_blank"
-          rel="noreferrer"
+          rel="noopener noreferrer"
         >
           Stevie Gill
+        </a>
+        {' | '}
+        <a
+          href="https://github.com/caffeinated-pixels/markdown-previewer-dev"
+          title="Markdown Previewer Github repo"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faGithubSquare} className="githubIcon" /> repo
         </a>
       </p>
     </footer>
